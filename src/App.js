4fc8f5b4ed1660375187595d78e15fdc71d00ace
Stephanie <stephcshea@gmail.com>
2018-08-25 20:00:10 -0400

    updating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import NavBar from './components/navbar'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/home'
+import About from './components/about'
+import Contact from './components/contact'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <NavBar />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+        <NavBar />
+        <Route exact path ='/' component={Home} />
+        <Route path ='/about' component={About} />
+        <Route path ='/contact' component={Contact} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
