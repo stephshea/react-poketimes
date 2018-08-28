f3e4e4b525f41cf3e053fc33f5f11b97268790e1
Stephanie <stephcshea@gmail.com>
2018-08-28 13:19:25 -0400

    getting and posting data thru axios

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">
-        Home
-      </h4> 
-      <p>Lorem impsum Abby and me! </p>
-    </div>
-  )
+class Home extends Component {
+  state = {
+    posts: [ ]
+  }
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => {
+      console.log(res);
+      this.setState({
+        posts: res.data.slice(0, 10)
+    }) 
+  })
 }
-
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? posts.map(post => {
+      return (
+        <div className="post card" key={post.id}>
+          <div className="card-content">
+            <span className="card-title">{post.title}</span>
+            <p>{post.body}</p>
+          </div>
+        </div>
+      )
+     }) : "there are no posts";
+    return (
+      <div className="container">
+        <h4 className="center">
+          Home
+        </h4> 
+        {postList}
+      </div>
+    )
+  }
+}
 export default Home
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link, NavLink, withRouter } from 'react-router-dom'
 
 const NavBar = (props) => {
-  setTimeout(() => {
-    props.history.push('./about')
-  }, 2000);
+  // setTimeout(() => {
+  //   props.history.push('./about')
+  // }, 2000);
 
   return (
     <nav className="nav-wrappe red darken-3">
