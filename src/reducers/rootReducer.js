51e8a5b7234024ff0ea0f1e0e48a5f69632ad046
Stephanie <stephcshea@gmail.com>
2018-09-10 13:06:59 -0400

    displaying posts using redux store

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,13 @@
 const initState = {
-  posts: []
+  posts: [
+    {id: '1', title: 'Abby 1', body: 'Abby is cute!'},
+    {id: '2', title: 'Abby 2', body: 'Abby is smart!'},
+    {id: '3', title: 'Abby 3', body: 'Abby is strong and svelte and healthy!'}
+  ]
 }
 
 const rootReducer = (state= initState, action) => {
   return state;
 }
 
-export defaults rootReducer
+export default rootReducer
